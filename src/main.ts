fe8385aa431805d5041dbbdc71dbbb8063ba516c
wilker818 <wilkerferreira818@gmail.com>
2022-12-27 00:37:06 -0300

    feat(global/style): added new style sheet to project

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,10 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
+import { store } from "./store";
 import "bootstrap";
 
+import "./assets/style/global.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 
